5ac1d794aea78262df21dc93b394574cbeec1f5c
Cadel Saszik <sazc0805@gmail.com>
2026-02-25 17:29:17 -0600

    LinkedList init

---
+++ src/main/java/edu/grinnell/csc207/generics/LinkedList.java
@@ -0,0 +1,82 @@
+package edu.grinnell.csc207.generics;
+
+public class LinkedList<T> {
+    private static class Node<U> {
+        private U value;
+        private Node<U> next;
+
+        public Node(U value, Node<U> next) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private Node<T> first;
+
+    public LinkedList() {
+        this.first = null;
+    }
+
+    public void add(T value) {
+        if (first == null) {
+            first = new Node<T>(value, null);
+        } else {
+            Node<T> cur = first;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = new Node<T>(value, null);
+        }
+    }
+
+    public int size() {
+        int sz = 0;
+        Node<T> cur = first;
+        while (cur != null) {
+            sz += 1;
+            cur = cur.next;
+        }
+        return sz;
+    }
+
+    public T get(int index) {
+        if (index < 0) {
+            throw new IndexOutOfBoundsException(index);
+        }
+        int origIndex = index;
+        Node<T> cur = first;
+        while (index > 0 && cur != null) {
+            index -= 1;
+            cur = cur.next;
+        }
+        if (cur == null) {
+            throw new IndexOutOfBoundsException(origIndex);
+        } else {
+            return cur.value;
+        }
+    }
+
+    public T remove(int index) {
+        if (index < 0 || (index == 0 && first == null)) {
+            throw new IndexOutOfBoundsException(index);
+        } else if (index == 0) {
+            T ret = first.value;
+            first = first.next;
+            return ret;
+        } else {
+            int origIndex = index;
+            Node<T> cur = first;
+            while (index > 1 && cur.next != null) {
+                index -= 1;
+                cur = cur.next;
+            }
+            if (cur.next == null) {
+                throw new IndexOutOfBoundsException(origIndex);
+            } else {
+                T ret = cur.next.value;
+                cur.next = cur.next.next;
+                return ret;
+            }
+        }
+    }
+}
