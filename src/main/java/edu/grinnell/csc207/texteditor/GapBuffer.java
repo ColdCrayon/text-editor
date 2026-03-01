82daf69821468c256387297e9a2ebfc0cf7709bc
johnkafumbe <kafumbejbrown@gmail.com>
2026-03-01 16:59:29 -0600

    Part 3 Complete

--- src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
@@ -4,35 +4,108 @@ package edu.grinnell.csc207.texteditor;
  * A gap buffer-based implementation of a text buffer.
  */
 public class GapBuffer implements Buffer {
+
+    private char[] data;
+    private int gapStart;
+    private int gapEnd;
+
+    public GapBuffer() {
+        this.data = new char[10];
+        this.gapStart = 0;
+        this.gapEnd = 10;
+    }
+
+    private void expand(){
+        int oldSize = data.length;
+        int newSize = oldSize * 2;
+        char[] newData = new char[newSize];
+
+        for(int i = 0; i < gapStart; i++){
+            newData[i] = data[i];
+        }
+
+        int afterSize = oldSize - gapEnd;
+        int newGapEnd = newSize - afterSize;
+        for(int i = 0; i < afterSize; i++){
+            newData[newGapEnd + i] = data[gapEnd + i];
+        }
+
+        this.data = newData;
+        this.gapEnd = newGapEnd;
+    }
+
+    @Override
     public void insert(char ch) {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        if(gapStart == gapEnd){
+            expand();
+        }
+        data[gapStart] = ch;
+        gapStart++;
     }
 
+    @Override
     public void delete() {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+       if (gapStart > 0){
+        gapStart--;
+       }
     }
 
+    @Override
     public int getCursorPosition() {
-        throw new UnsupportedOperationException("Unimplemented method 'getCursorPosition'");
+        return gapStart;
     }
 
+    @Override
     public void moveLeft() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveLeft'");
-    }
+        if (gapStart > 0){
+            data[gapEnd - 1] = data[gapStart - 1];
+            gapStart--;
+            gapEnd--;
+        }
+    }  
 
+    @Override
     public void moveRight() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveRight'");
+       if (gapEnd < data.length){
+           data[gapStart] = data[gapEnd];
+           gapStart++;
+           gapEnd++;
+       }
     }
 
+    @Override
     public int getSize() {
-        throw new UnsupportedOperationException("Unimplemented method 'getSize'");
+       int gapLength = gapEnd - gapStart;
+       return data.length - gapLength;
     }
 
+    @Override
     public char getChar(int i) {
-        throw new UnsupportedOperationException("Unimplemented method 'getChar'");
+        if (i < 0 || i >= getSize()){
+            throw new IndexOutOfBoundsException();
+        }
+        if (i < gapStart){
+            return data[i];
+        } else {
+            int gapLength = gapEnd - gapStart;
+            return data[i + gapLength];
+        }
     }
 
+    @Override
     public String toString() {
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+        int actualSize = getSize();
+        char[] result = new char[actualSize];
+
+        for(int i = 0; i < gapStart; i++){
+            result[i] = data[i];
+        }
+
+        int afterSize = data.length - gapEnd;
+        for(int i = 0; i < afterSize; i++){
+            result[gapStart + i] = data[gapEnd + i];
+        }
+
+        return new String(result);
     }
 }
--- src/test/java/edu/grinnell/csc207/texteditor/GapBufferTests.java
+++ src/test/java/edu/grinnell/csc207/texteditor/GapBufferTests.java
@@ -3,10 +3,62 @@ package edu.grinnell.csc207.texteditor;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.DisplayNameGenerator.Simple;
+//import org.junit.jupiter.api.DisplayNameGenerator.Simple;
 
 import net.jqwik.api.ForAll;
 import net.jqwik.api.Property;
 
 public class GapBufferTests {
+/**
+     * Test insertions and cursor movements.
+     */
+    @Test
+    public void testBasicOperations() {
+        GapBuffer buffer = new GapBuffer();
+        buffer.insert('H');
+        buffer.insert('e');
+        buffer.insert('l');
+        buffer.insert('l');
+        buffer.insert('o');
+        
+        assertEquals("Hello", buffer.toString());
+        assertEquals(5, buffer.getCursorPosition());
+
+        buffer.moveLeft();
+        buffer.moveLeft();
+        buffer.delete(); // Deletes 'l'
+        
+        assertEquals("Helo", buffer.toString());
+        assertEquals(2, buffer.getCursorPosition());
+    }
+    /**
+     * Test to force the array to expnd becuase of inserting the 11th char
+     */
+
+    @Test
+    public void testExpansion(){
+        GapBuffer buffer = new GapBuffer();
+        String initial = "1234567890";
+        for (char c : initial.toCharArray()) {
+            buffer.insert(c);
+        }
+        
+        buffer.insert('!');
+        
+        assertEquals("1234567890!", buffer.toString());
+        assertEquals(11, buffer.getSize());
+    }
+
+    @Property
+    void insertDeleteTest (@ForAll char c) {
+        GapBuffer buffer = new GapBuffer();
+        buffer.insert('s');
+        String original = buffer.toString();
+        
+        buffer.insert(c);
+        buffer.delete();
+        
+        assertEquals(original, buffer.toString());
+    }
+
 }
