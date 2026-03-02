4d3acafd69409be8b5ca306fa9ba591be41817fe
johnkafumbe <kafumbejbrown@gmail.com>
2026-03-01 20:13:20 -0600

    Text-editor Complete

--- src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
@@ -9,12 +9,19 @@ public class GapBuffer implements Buffer {
     private int gapStart;
     private int gapEnd;
 
+    /**
+     * Constructs an empty GapBuffer with an intial capacitu of 10
+     */
     public GapBuffer() {
         this.data = new char[10];
         this.gapStart = 0;
         this.gapEnd = 10;
     }
 
+    /**
+     * Doubles the size of the backing array when the gap is full.
+     * Moves the text after the gap to the end of the new, larger array.
+     */
     private void expand(){
         int oldSize = data.length;
         int newSize = oldSize * 2;
@@ -34,6 +41,9 @@ public class GapBuffer implements Buffer {
         this.gapEnd = newGapEnd;
     }
 
+    /**
+     * Inserts a character at the current cursor position
+     */
     @Override
     public void insert(char ch) {
         if(gapStart == gapEnd){
@@ -43,6 +53,9 @@ public class GapBuffer implements Buffer {
         gapStart++;
     }
 
+    /**
+     * Deletes the character to the left of the cursor
+     */
     @Override
     public void delete() {
        if (gapStart > 0){
@@ -50,11 +63,18 @@ public class GapBuffer implements Buffer {
        }
     }
 
+    /**
+     * Returns the position the cursor
+     */
     @Override
     public int getCursorPosition() {
         return gapStart;
     }
 
+    /**
+     * Moves the cursor one position to the left by shifting a character
+     * from the left side of the gap to the right side.
+     */
     @Override
     public void moveLeft() {
         if (gapStart > 0){
@@ -64,6 +84,10 @@ public class GapBuffer implements Buffer {
         }
     }  
 
+     /**
+     * Moves the cursor one position to the right by shifting a character
+     * from the right side of the gap to the left side.
+     */
     @Override
     public void moveRight() {
        if (gapEnd < data.length){
@@ -73,12 +97,18 @@ public class GapBuffer implements Buffer {
        }
     }
 
+    /**
+     * Returns the total number of characters in the buffer
+     */
     @Override
     public int getSize() {
        int gapLength = gapEnd - gapStart;
        return data.length - gapLength;
     }
 
+    /**
+     * Returns the character at index i, jumping over the gap if necessary.
+     */
     @Override
     public char getChar(int i) {
         if (i < 0 || i >= getSize()){
@@ -92,6 +122,9 @@ public class GapBuffer implements Buffer {
         }
     }
 
+    /**
+     * Converts the buffer content into a String, excluding the gap.
+     */
     @Override
     public String toString() {
         int actualSize = getSize();
